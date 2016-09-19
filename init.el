34fbd46911bef1b6ad79189a7605072855795bd5
Evgeny Cherkashin (at mux.irnok.net) <eugeneai@irnok.net>
2016-09-19 09:15:17 +0800

    Removed redundant variable.

--- init.el
+++ init.el
@@ -295,7 +295,7 @@ executes.
 before packages are loaded. If you are unsure, you should try in setting them in
 `dotspacemacs/user-config' first."
   ;(setq-default dotspacemacs-themes '(eugeneai-test))
-  (setq powerline-default-separator 'utf-8)
+  ;(setq powerline-default-separator 'utf-8)
   (setq paradox-github-token  "fa03a229cda23b78e9682d6cf498b8ee20fca81e")
   )
 
@@ -371,7 +371,7 @@ you should place your code here."
     (powerline rust-mode pyenv-mode pythonic spinner hydra markdown-mode json-snatcher json-reformat multiple-cursors js2-mode parent-mode projectile request haml-mode gitignore-mode fringe-helper git-gutter+ git-gutter gh marshal logito pcache ht f flycheck pkg-info epl flx grizzl magit magit-popup git-commit with-editor smartparens iedit anzu highlight php-mode web-completion-data dash-functional tern pos-tip jedi-core python-environment epc ctable concurrent deferred company yasnippet packed auctex s dash helm avy helm-core async auto-complete popup package-build bind-key bind-map evil ws-butler window-numbering which-key web-mode web-beautify w3m volatile-highlights vi-tilde-fringe use-package toml-mode tagedit sql-indent spacemacs-theme spaceline smooth-scrolling smeargle slim-mode scss-mode sass-mode restart-emacs rainbow-delimiters racer quelpa pyvenv pytest pyenv-mode-auto py-yapf popwin pip-requirements phpunit phpcbf php-extras php-auto-yasnippets persp-mode pcre2el paradox pandoc-mode page-break-lines ox-pandoc orgit open-junk-file neotree move-text mmm-mode markdown-toc markdown-mode+ magit-gitflow magit-gh-pulls macrostep lua-mode lorem-ipsum linum-relative leuven-theme less-css-mode json-mode js2-refactor js-doc jade-mode info+ indent-guide ido-vertical-mode hy-mode hungry-delete htmlize hl-todo highlight-parentheses highlight-numbers highlight-indentation help-fns+ helm-themes helm-swoop helm-pydoc helm-projectile helm-mode-manager helm-make helm-gitignore helm-flyspell helm-flx helm-descbinds helm-css-scss helm-company helm-c-yasnippet helm-ag goto-last-change google-translate golden-ratio github-clone github-browse-file gitconfig-mode gitattributes-mode git-timemachine git-messenger git-link git-gutter-fringe git-gutter-fringe+ gist gh-md flycheck-rust flycheck-pos-tip flycheck-dmd-dub flx-ido fish-mode fiplr fill-column-indicator fancy-battery expand-region exec-path-from-shell evil-visualstar evil-tutor evil-surround evil-search-highlight-persist evil-numbers evil-nerd-commenter evil-mc evil-matchit evil-magit evil-lisp-state evil-indent-plus evil-iedit-state evil-exchange evil-escape evil-args evil-anzu eval-sexp-fu emmet-mode elisp-slime-nav drupal-mode disaster diff-hl define-word d-mode cython-mode cursor-chg company-web company-tern company-statistics company-racer company-quickhelp company-jedi company-c-headers company-auctex color-theme coffee-mode cmake-mode clean-aindent-mode clang-format buffer-move bracketed-paste auto-yasnippet auto-highlight-symbol auto-dictionary auto-compile auctex-latexmk aggressive-indent ag adaptive-wrap ace-window ace-link ace-jump-mode ace-jump-helm-line ac-ispell)))
  '(paradox-automatically-star t)
  '(paradox-github-token t t)
- '(powerline-default-separator (quote utf-8))
+ ;'(powerline-default-separator (quote utf-8))
  '(safe-local-variable-values
    (quote
     ((TeX-auto-save . t)
